ededf85a39c8a63d885a52667e7339f6af390ad3
jessthayer <63027836+jessthayer@users.noreply.github.com>
2020-04-18 10:22:28 -0500

    created dynamic effort cost function with step length as constraint, not optimization target

--- objfunSS_v2.m
+++ objfunSS_v2.m
@@ -5,26 +5,35 @@ simulator.Parameters = p;
 simulator = sim(simulator);
 
 % obtain the step length optimization results
-StepLength21 = get(simulator.LoggedData,'PredictedStepLength');
-VerticalPos21 = get(simulator.LoggedData,'PredictedVerticalPos');
-VerticalPosEnd21 = get(simulator.LoggedData,'PredictedVerticalPosEnd');
-
+stepLength21 = get(simulator.LoggedData,'PredictedStepLength');
 dynamic_effort21 = get(simulator.LoggedData, 'Dynamic_Effort');
-
-% define design parameters (from Internal_MPC_Model_21)
-[p(1).Value p(2).Value p(3).Value p(4).Value] 
-
-% define desired step length
-StepLength = 0.725; % [meters]
+VerticalPos21 = get(simulator.LoggedData,'PredictedVerticalPos');
+swingToeZ = get(simulator.LoggedData,'swing_toe_z');
+HCflag = get(simulator.LoggedData,'Flag');
 
 % define bodymass
-bodymass = 86.2; % [kg]
+bodymass = 86.2; % [kg] %THIS SHOULD BE CALLED FROM THE MODEL
+
+for i = length(HCflag)
+    if HCflag(i) == 1
+        stepLength = stepLength21(i);
+        return
+    else
+        stepLength = -0.7;
+    end
+end
+
+% define design parameters
+[p(1).Value p(2).Value p(3).Value p(4).Value];
+
+% define desired step length
+stepLength_target = 0.700; % [meters]
 
 % define dynamic effort
 dynamic_effort = sum(dynamic_effort21(:))
 
 % define design objectives
-design.F = 2*(StepLength-StepLength21)^2*100000 + 0.1*dynamic_effort; %F — Value of the cost (objective) evaluated at p. The solver minimizes F.
-design.Cleq = [-VerticalPos21;VerticalPosEnd21-0.01]; %Cleq — Value of the nonlinear inequality constraint violations evaluated at p. The solver satisfies Cleq(p) <= 0.
+design.F = dynamic_effort %F — Value of the cost (objective) evaluated at p. The solver minimizes F.
+design.Cleq = [-VerticalPos21; -swingToeZ; ((stepLength_target-stepLength)-0.05*stepLength)]; %Cleq — Value of the nonlinear inequality constraint violations evaluated at p. The solver satisfies Cleq(p) <= 0.
 
 end
